b7e5c7437eb4962ae349534884bc58fa665df9e2
Malte Hallgreen <malhal@DTU-FVFF80KGQ05P.local>
2021-03-07 17:40:08 +0100

    indel fix friday

--- kmaVCF2VCF.py
+++ kmaVCF2VCF.py
@@ -9,21 +9,21 @@ import gzip
 
 parser = argparse.ArgumentParser(description='vcfFilter')
 parser.add_argument('-vcf', action="store", type=str, required=True, dest='vcf', default="", help='kma vcf')
-
+parser.add_argument('-d', action="store", type=int, dest='depth', default=50, help='Depth threshold for including a position')
+#parser.add_argument('-maj_s', action="store", type=float, dest='maj_s', default=0.7, help='Support for accepting majority variant')
+parser.add_argument('-min_s', action="store", type=float, dest='min_s', default=0.15, help='Support for accepting minority variant')
 
 args = parser.parse_args()
 
-vcfheader = "##reference=/home/share/pkrisz5-sarscov2veo/data/ref/sars2/NC_045512.2.fa\n##INFO=<ID=DP,Number=1,Type=Integer,Description=\"Raw Depth\">\n##INFO=<ID=AF,Number=1,Type=Float,Description=\"Allele Frequency\">\n##INFO=<ID=SB,Number=1,Type=Integer,Description=\"Phred-scaled strand bias at this position\">\n##INFO=<ID=DP4,Number=4,Type=Integer,Description=\"Counts for ref-forward bases, ref-reverse, alt-forward and alt-reverse bases\">\n##INFO=<ID=INDEL,Number=0,Type=Flag,Description=\"Indicates that the variant is an INDEL.\">\n##INFO=<ID=CONSVAR,Number=0,Type=Flag,Description=\"Indicates that the variant is a consensus variant (as opposed to a low frequency variant).\">\n##INFO=<ID=HRUN,Number=1,Type=Integer,Description=\"Homopolymer length to the right of report indel position\">"
-basepairDict = dict()
-basepairDict['A'] = 'T'
-basepairDict['T'] = 'A'
-basepairDict['C'] = 'G'
-basepairDict['G'] = 'C'
+vcfheader = "##reference=/home/share/pkrisz5-sarscov2veo/data/ref/sars2/NC_045512.2.fa\n##INFO=<ID=DP,Number=1,Type=Integer,Description=\"Raw Depth\">\n##INFO=<ID=AF,Number=1,Type=Float,Description=\"Allele Frequency\">\n##INFO=<ID=SB,Number=1,Type=Integer,Description=\"Phred-scaled strand bias at this position\">\n##INFO=<ID=AD6,Number=6,Type=Integer,Description=\"Count of all alternative alleles: A,C,G,T,N,-\">\">\n##INFO=<ID=INDEL,Number=0,Type=Flag,Description=\"Indicates that the variant is an INDEL.\">\n##INFO=<ID=CONSVAR,Number=0,Type=Flag,Description=\"Indicates that the variant is a consensus variant (as opposed to a low frequency variant).\">\n##INFO=<ID=HRUN,Number=1,Type=Integer,Description=\"Homopolymer length to the right of report indel position\">"
 vcf = args.vcf
+depth = args.depth
+min_s = args.min_s
+
 
 def main():
     vcflist = loadVCF(vcf)
-    vcflist = convertVCF(vcflist)
+    vcflist = convertVCF(vcflist, min_s)
     vcflist = indelTag(vcflist)
     print (vcfheader)
     for i in range(len(vcflist)):
@@ -37,72 +37,68 @@ def indelTag(vcflist):
         newVCFlist.append(position)
     return newVCFlist
 
-
-def covertBaseCalls(vcflist):
+def convertVCF(vcflist, min_s):
     newVCFlist = []
     for position in vcflist:
-        dp4 = [0,0,0,0]
-        ad6 = position[7].split(";")[5][4:].split(",")
-        if position[3] == "A":
-            dp4[0] = int(ad6[0])/2
-            dp4[1] = int(ad6[0])/2
-        elif position[3] == "C":
-            dp4[0] = int(ad6[1])/2
-            dp4[1] = int(ad6[1])/2
-        elif position[3] == "G":
-            dp4[0] = int(ad6[2])/2
-            dp4[1] = int(ad6[2])/2
-        elif position[3] == "T":
-            dp4[0] = int(ad6[3])/2
-            dp4[1] = int(ad6[3])/2
-        elif position[3] == "N":
-            dp4[0] = int(ad6[4])/2
-            dp4[1] = int(ad6[4])/2
-        elif position[3] == "-":
-            dp4[0] = int(ad6[5])/2
-            dp4[1] = int(ad6[5])/2
-
-        if position[4].upper() == "A":
-            dp4[2] = int(ad6[0])/2
-            dp4[3] = int(ad6[0])/2
-        elif position[4].upper() == "C":
-            dp4[2] = int(ad6[1])/2
-            dp4[3] = int(ad6[1])/2
-        elif position[4].upper() == "G":
-            dp4[2] = int(ad6[2])/2
-            dp4[3] = int(ad6[2])/2
-        elif position[4].upper() == "T":
-            dp4[2] = int(ad6[3])/2
-            dp4[3] = int(ad6[3])/2
-        elif position[4].upper() == "N":
-            dp4[2] = int(ad6[4])/2
-            dp4[3] = int(ad6[4])/2
-        elif position[4].upper() == "-":
-            dp4[2] = int(ad6[5])/2
-            dp4[3] = int(ad6[5])/2
-        for i in range(len(dp4)):
-            dp4[i] = str(dp4[i])
-        vcfinfo = position[7].split(";")
-        vcfinfo[5] = "DP4=" + ",".join(dp4)
-        position[7] = ";".join(vcfinfo)
-        newVCFlist.append(position)
+        vcfInfo = position[7].split(";")
+        if float(vcfInfo[0][3:]) >= depth:
+            positionType = indentifyPositionType(position)
+            minorityVariant, minority_depth = calculateMinor(vcfInfo)
+            newVCFlist = handlePosition(position, positionType, minorityVariant, minority_depth, min_s, newVCFlist)
     return newVCFlist
 
-
-def convertVCF(vcflist):
-    newVCFlist = []
-    for position in vcflist:
-        if position[1] != "0":
-            if position[4] == "<->": #deletion:
-                if position[3] != "<->":
-                    newVCFlist[-1][3] += position[3]
-            else:
+def handlePosition(position, positionType, minorityVariant, minority_depth, min_s, newVCFlist):
+    indelFlag = False
+    if positionType == "variant_majority":
+        if minority_depth >= min_s:
+            if minorityVariant != "-":
+                position[4] = minorityVariant
                 newVCFlist.append(position)
+            else:
+                if indelFlag == False:
+                    newVCFlist[-1][4] += position[4]
         else:
-            if position[3] == "<->" and position[4] != "<->": #Insertion
-                newVCFlist[-1][4] += position[4]
+            newVCFlist.append(position)
+    elif positionType == "deletion_majority":
+        newVCFlist[-1][3] += position[3]
+    elif positionType == "insertion_majority":
+        newVCFlist[-1][4] += position[4]
+    elif positionType == "minor_insertion":
+        pass
+    else:
+        newVCFlist.append(position)
 
     return newVCFlist
+
+def calculateMinor(vcfInfo):
+    variants = vcfInfo[5][4:]
+    variantCount = variants.split(",")
+    sort_variantCount = []
+    for i in range(len(variantCount)):
+        sort_variantCount.append(int(variantCount[i]))
+    sort_variantCount.sort()
+    minority_index = variantCount.index(str(sort_variantCount[-2]))
+    dp = int(vcfInfo[0][3:])
+    variantList = ['A', 'C', 'G', 'T', 'N', '-']
+    minorityVariant = variantList[minority_index]
+    minority_depth = int(sort_variantCount[-2]) / dp
+    return minorityVariant, minority_depth
+
+def indentifyPositionType(position):
+    if position[1] != "0":
+        if position[4] == "<->":  # deletion:
+            positionType = "deletion_majority"
+        else:
+            positionType = "variant_majority"
+    else:
+        if position[3] == "<->" and position[4] != "<->":  # Normal Insertion majority
+            positionType = "insertion_majority"
+
+        else:
+            positionType = "minor_insertion"
+    return positionType
+
+
 
 
 
